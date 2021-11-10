cee8013a89a7d5f5f12e402661e512c1e03901f1
Gyujae <wjdrbwo1206@naver.com>
2021-11-10 21:21:23 +0900

    first commit

---
+++ src/Router.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Coin from "./Routes/Coin";
+import Coins from "./Routes/Coins";
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/:coinId">
+          <Coin />
+        </Route>
+        <Route path="/">
+          <Coins />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
+};
+
+export default Router;
